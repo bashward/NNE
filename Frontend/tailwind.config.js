2e5ef79b74c67a9ffe48cf9a8b167dcbdb1f42da
Nikhil Thakur <101515643+bashward@users.noreply.github.com>
2024-12-17 15:35:02 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,21 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    screens: {
-      'xs': '400px',
-
-      'sm': '576px',
-      
-      'md': '960px',
-      
-      'lg': '1440px',
-    },
     extend: {},
   },
   plugins: [],
 }
+
 
 
