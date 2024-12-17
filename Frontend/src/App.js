4294ffc53f1711724db518a49c12f55457ee713d
Nikhil Thakur <101515643+bashward@users.noreply.github.com>
2024-12-17 15:12:16 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,26 @@
-import Navbar from "./Components/Navbar";
-import {Route,Routes} from 'react-router-dom'
-import Home from "./Pages/Home";
-import Footer from "./Components/Footer";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import ScrollToTop from "./Components/ScrollToTop"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Products from './Pages/Products';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <>
-    <ScrollToTop/>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/aboutus" element={<About/>}/>
-      <Route path="/contactus" element={<Contact/>}/>
-    </Routes>
-    <Footer/>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
