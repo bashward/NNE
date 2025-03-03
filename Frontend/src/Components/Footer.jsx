65e4d4966a8cb835178a482707c0542393b6ed6a
Nikhil Thakur <101515643+bashward@users.noreply.github.com>
2025-03-03 19:23:31 +0530

    Update Footer.jsx

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="py-4 text-center text-sm text-gray-500 bg-gray-100 flex flex-col items-center">
       <p>© {new Date().getFullYear()} NNE. All rights reserved.</p>
       <a
-        href="https://www.linkedin.com/company/nne" // 🔹 Replace with actual LinkedIn URL
+        href="https://www.linkedin.com/company/n-n-magnetics-and-electronics-pvt-ltd" // 🔹 Replace with actual LinkedIn URL
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-600 hover:text-blue-800 mt-2 flex items-center space-x-2"
