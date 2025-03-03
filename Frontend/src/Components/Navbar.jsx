676b1c84076b791686eb944e2dc822d76d82830a
Nikhil Thakur <101515643+bashward@users.noreply.github.com>
2025-03-03 17:34:02 +0530

    Update Navbar.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
           <img 
             src={logo} 
             alt="Logo" 
-            className="w-16 md:w-8 h-auto" // Adjusted for mobile & desktop
+            className="w-24 md:w-8 h-auto" // Adjusted for mobile & desktop
           />
         </Link>
 
